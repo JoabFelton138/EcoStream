0abeaa503ad16c462d9ecc5c5c53ef8fad509425
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-04-22 10:15:21 +0100

    test: unit test added for kpi card render

---
+++ eco-stream/components/widgets/kpi-cards.test.tsx
@@ -0,0 +1,28 @@
+import { mockData } from "../../test/mockData";
+import { describe, expect, it } from "vitest";
+import { KpiCards } from "./kpi-cards";
+import { render, screen } from "@testing-library/react";
+
+describe("KpiCards", () => {
+    it("should render the KPI cards with computed values", () => {
+        render(<KpiCards overflowData={mockData} />)
+        expect(screen.getByText('Total Active Spills')).toBeInTheDocument();
+        expect(screen.getAllByText('7')).toHaveLength(2);
+
+        expect(screen.getByText('Number of Companies Dumping')).toBeInTheDocument();
+        expect(screen.getByText('4')).toBeInTheDocument();
+        
+        expect(screen.getByText('Company With Highest Number of Spills')).toBeInTheDocument();
+        expect(screen.getByText('thames water')).toBeInTheDocument();
+        expect(screen.getByText('2 Active Spills')).toBeInTheDocument();
+        
+        expect(screen.getByText('Dry Weather Spills')).toBeInTheDocument();
+        
+        expect(screen.getByText('Longest Active Spill')).toBeInTheDocument();
+        expect(screen.getByText('At last monitoring update: 6d 9h 51m')).toBeInTheDocument();
+        expect(screen.getByText('dyan beck - united utilities')).toBeInTheDocument();
+        
+        expect(screen.getByText('Number of Rivers Polluted')).toBeInTheDocument();
+        screen.debug();
+    });
+});
